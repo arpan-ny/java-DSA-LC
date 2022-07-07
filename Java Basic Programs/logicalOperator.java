931b3a75ceb7c7310be5791eeb86f0a1375fb23c
Arpan Nyati <nyati.arpan@gmail.com>
2022-07-07 21:36:37 +0530

    Create logicalOperator.java

---
+++ Java Basic Programs/logicalOperator.java
@@ -0,0 +1,5 @@
+// Logic Operators in Java
+// Example
+//( Logical and ) && Returns true if both statements are true      Eg. x<5 && x<10
+//  (Logical or) || Returns true if one of the statemnts is true   Eg. x<5 || x<4
+// (Logical not) ! Reverse the result, returns false if the result is true  Eg. !(x < 5 && x < 10)
