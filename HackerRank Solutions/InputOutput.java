d031055b0e16402316cef0174a93b6342631cb6b
Arpan Nyati <nyati.arpan@gmail.com>
2022-07-09 22:32:32 +0530

    Update InputOutput.java
    Updated Explanation

--- HackerRank Solutions/InputOutput.java
+++ HackerRank Solutions/InputOutput.java
@@ -1,5 +1,4 @@
 // HackerRank
-//
 //https://www.hackerrank.com/challenges/java-stdin-stdout/problem
 
 import java.util.Scanner;
@@ -20,6 +19,23 @@ public class InputOutput {
         the user thinking that the user has entered the enter key.(that's we get empty output) .
         Unlike C, there is no fflush() to clean buffer, so we have to flush by not taking it in variable.
         Explanation by @Akshatsaxena21
+        */
+        /*
+        Invisible "\n" newlines
+
+        At the end of each line of the provided input, there is an invisible "\n" which is called a newline character.
+        This basically represents what happens when you press the "Enter" key when typing.
+
+        1) The first scan.nextInt() grabs the first "int" from the input. It still leaves the "\n" on that line.
+
+        2) The second scan.nextDouble() skips over that "\n" and grabs the first "double" it sees.
+
+        3) Now is the tricky part. It turns out scan.nextLine() works differently
+        than scan.nextInt() and scan.nextDouble() in that it doesn't skip over any "\n" newline characters.
+        What it does is it reads and returns all characters until it reaches the "\n" (which it gets rid of), and stops.
+        In the scenario above, since "\n" is the first thing we see, we get an empty String back on our 1st call to scan.nextLine().
+
+        4) Our next call to scan.nextLine() will start on line 3 of input and grab the String we want.
         */
         String s = scan.nextLine();
         scan.close();
