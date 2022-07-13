a5d547a8e0178f6778112bd87d4968b940a99a6f
Arpan Nyati <nyati.arpan@gmail.com>
2022-07-13 22:38:07 +0530

    Create FinalVal.java

---
+++ Java Basic Programs/FinalVal.java
@@ -0,0 +1,13 @@
+class FinalVal {
+    public int finalValueAfterOperations(String[] operations) {
+        int ans = 0;
+        for(int i=0; i<operations.length;i++){
+            if(operations[i].charAt(1)=='+') {
+                ans++;
+                }else {
+                ans--;
+            }
+        }
+        return ans;
+    }
+}
